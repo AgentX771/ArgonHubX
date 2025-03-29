ac572d89d777bca22c8703a8ad8ec0133e357a33
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-03-29 19:06:57 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,8 @@
-print("Load!")
+local superCoolGameID = 87854376962069
+local myEpicGameID = game.PlaceId
+
+if myEpicGameID == superCoolGameID then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Glass%20Bridge.lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/IDMusic.lua"))()
+end
