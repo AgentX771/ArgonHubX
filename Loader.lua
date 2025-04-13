8f1d68ac345c5733deb42d18a23ea68b215f86b8
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-04-13 18:26:41 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,6 @@ local plr = game:GetService("Players").LocalPlayer
 if plr.Name == "cooluwuboy123sigma" then
     plr:Kick("Banned for mocking the leader")
 end
-
 local superCoolGameID = 87854376962069
 local myEpicGameID = game.PlaceId
 
