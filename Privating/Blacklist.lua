497e146dcfdc9bb32345f6ab3fb4571993b2c6c1
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-04-14 00:08:53 -0300

    Update Blacklist.lua

--- Privating/Blacklist.lua
+++ Privating/Blacklist.lua
@@ -1,5 +1,6 @@
 local bannedPlayers = {
     [8016396684] = "Mocking the Creator",
+    [8306727532] = "Mocking the Creator",
 }
 
 local baseMessage = "Argon Hub X has determined that his behavior on the platform has been inappropriate and has been banned for: "
