26ec2e215b95d38cb8af8bc8c6f69a0cf80e958a
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-04-14 17:46:44 -0300

    Update Version.lua

--- Privating/Version.lua
+++ Privating/Version.lua
@@ -1,6 +1,6 @@
 local _env = getgenv and getgenv() or {}
 
-local Ver = "Version: 1.4.2"
+local Ver = "Version: 2.4.2"
 local JoinDiscord = "https://discord.gg/G2WgRW295J"
 
 _env.Discord = JoinDiscord
