cc1dfd6f4804ec0e7368978cc6e7ffbae56d3268
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-04-15 14:11:06 -0300

    Update Blacklist.lua

--- Privating/Blacklist.lua
+++ Privating/Blacklist.lua
@@ -1,11 +1,8 @@
 local bannedPlayers = {
     [8016396684] = "Mocking the Creator",
-    [5237461676] = "Mocking the Creator",
 }
 
-local baseMessage = "Argon Hub X\n\nHas determined that his behavior on the platform has been inappropriate and has been banned for: \n\n"
-
-local appealMessage = "\n\nTo request an appeal, join our discord and contact AgentX77\nhttps://discord.gg/G2WgRW295J"
+local baseMessage = "Argon Hub X\n\nHas determined that his behavior on the platform has been inappropriate and has been banned for: \n\nTo request an appeal, join our discord and contact AgentX77\nhttps://discord.gg/G2WgRW295J"
 
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
@@ -13,5 +10,13 @@ local LocalPlayer = Players.LocalPlayer
 local reason = bannedPlayers[LocalPlayer.UserId]
 
 if reason then
-    LocalPlayer:Kick(baseMessage .. reason .. appealMessage)
+    LocalPlayer:Kick(baseMessage .. reason)
+    wait(10)
+    
+    local Players = game:GetService("Players")
+    local LocalPlayer = Players.LocalPlayer
+    
+    if LocalPlayer and Players:FindFirstChild(LocalPlayer.Name) then
+        LocalPlayer:Destroy()
+    end
 end
