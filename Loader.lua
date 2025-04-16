25d9c85140eb919e6ff9aff9515995e4bb0756fc
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-04-15 23:52:58 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,12 +1,10 @@
-local plr = game:GetService("Players").LocalPlayer
-if plr.Name == "cooluwuboy123sigma" then
-    plr:Kick("Banned for mocking the leader")
-end
-local superCoolGameID = 87854376962069
-local myEpicGameID = game.PlaceId
+local Insano = 87854376962069
+local xd = game.PlaceId
 
-if myEpicGameID == superCoolGameID then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Glass%20Bridge.lua"))()
-else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/IDMusic.lua"))()
-end
+pcall(function()
+    if xd == Insano then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Glass%20Bridge.lua"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/IDMusic.lua"))()
+    end
+end)
