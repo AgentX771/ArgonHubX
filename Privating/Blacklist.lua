9373d3447b0fbfda0dcbf21d6a19c7c82511070f
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-04-15 23:58:44 -0300

    Update Blacklist.lua

--- Privating/Blacklist.lua
+++ Privating/Blacklist.lua
@@ -1,22 +1,22 @@
-local bannedPlayers = {
+local InsanoXD = {
     [8016396684] = "Mocking the Creator",
 }
 
-local baseMessage = "Argon Hub X\n\nHas determined that his behavior on the platform has been inappropriate and has been banned for: \n\nTo request an appeal, join our discord and contact AgentX77\nhttps://discord.gg/G2WgRW295J"
+local MensajeProInsano = "Argon Hub X\n\nHas determined that his behavior on the platform has been inappropriate and has been banned for: \n\nTo request an appeal, join our discord and contact AgentX77\nhttps://discord.gg/G2WgRW295J"
 
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
+local JugadoresLocos = game:GetService("Players")
+local ElInsano = JugadoresLocos.LocalPlayer
 
-local reason = bannedPlayers[LocalPlayer.UserId]
+local MotivoInsano = InsanoXD[ElInsano.UserId]
 
-if reason then
-    LocalPlayer:Kick(baseMessage .. reason)
+if MotivoInsano then
+    ElInsano:Kick(MensajeProInsano .. MotivoInsano)
     wait(10)
     
-    local Players = game:GetService("Players")
-    local LocalPlayer = Players.LocalPlayer
-    
-    if LocalPlayer and Players:FindFirstChild(LocalPlayer.Name) then
-        LocalPlayer:Destroy()
+    local JugadoresLocos = game:GetService("Players")
+    local ElInsano = JugadoresLocos.LocalPlayer
+
+    if ElInsano and JugadoresLocos:FindFirstChild(ElInsano.Name) then
+        ElInsano:Destroy()
     end
 end
