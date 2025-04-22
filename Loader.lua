f80549b91ae0e1c22b4af69dbe2663a04adfa901
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-04-21 21:03:20 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,12 @@
 local Insano = 87854376962069
+local Evade = 9872472334
 local xd = game.PlaceId
 
 pcall(function()
     if xd == Insano then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Glass%20Bridge.lua"))()
+    elseif xd == Evade then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Evade.lua"))()
     else
         loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/IDMusic.lua"))()
     end
