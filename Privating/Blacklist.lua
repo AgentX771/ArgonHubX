8892b135cd2fa305be6f01bbd296fc5f7568ccc5
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-05-15 20:48:40 -0600

    Update Blacklist.lua

--- Privating/Blacklist.lua
+++ Privating/Blacklist.lua
@@ -2,7 +2,7 @@ local BanReasonsByUserId = {
     [8016396684] = "Mocking the Creator",
 }
 
-local BanMessage = "Argon Hub X\n\nHas determined that your behavior on the platform has been inappropriate and you have been banned for: \n\nTo request an appeal, join our Discord and contact AgentX77\nhttps://discord.gg/G2WgRW295J"
+local BanMessage = "Argon Hub X\n\nHas determined that your behavior on the platform has been inappropriate and you have been banned for: \n\nTo request an appeal, join our Discord and contact AgentX77\nhttps://discord.gg/G2WgRW295J\n\nLink copied to your clipboard."
 
 local PlayersService = game:GetService("Players")
 local LocalPlayer = PlayersService.LocalPlayer
@@ -11,6 +11,7 @@ local BanReason = BanReasonsByUserId[LocalPlayer.UserId]
 
 if BanReason then
     LocalPlayer:Kick(BanMessage .. BanReason)
+    setclipboard("https://discord.gg/G2WgRW295J")
     wait(10)
 
     local PlayersService = game:GetService("Players")
