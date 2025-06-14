d6175345af46677292893f5a82538a562a7629d0
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-06-13 19:33:39 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,12 +1,19 @@
 local Insano = 87854376962069
 local Evade = 9872472334
-local xd = game.PlaceId
+local BladeBall1 = 13772394625
+local BladeBall2 = 15144787112
+local BladeBall3 = 15234596844
+local BladeBall4 = 16281300371
+local BladeBall5 = 16044264830
+local GameID = game.PlaceId
 
 pcall(function()
-    if xd == Insano then
+    if GameID == Insano then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Glass%20Bridge.lua"))()
-    elseif xd == Evade then
+    elseif GameID == Evade then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Evade.lua"))()
+    elseif GameID == BladeBall1 or GameID == BladeBall2 or GameID == BladeBall3 or GameID == BladeBall4 or GameID == BladeBall5 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Blade%20Ball.lua"))()
     else
         loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/IDMusic.lua"))()
     end
