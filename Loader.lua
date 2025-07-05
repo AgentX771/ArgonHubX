80a4f2bea50c25e9670c6771f891113e3d765240
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-07-05 01:45:17 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,19 @@
-local Insano = 87854376962069
+local Insano = 87854376962069 
 local Evade = 9872472334
+
 local BladeBall1 = 13772394625
 local BladeBall2 = 15144787112
 local BladeBall3 = 15234596844
 local BladeBall4 = 16281300371
 local BladeBall5 = 16044264830
+local BladeBall6 = 14732610803
+local BladeBall7 = 15131065025
+local BladeBall8 = 15264892126
+local BladeBall9 = 14915220621
+local BladeBall10 = 15517169103
+local BladeBall11 = 15509350986
+local BladeBall12 = 16581648071
+
 local GameID = game.PlaceId
 
 pcall(function()
@@ -12,7 +21,11 @@ pcall(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Glass%20Bridge.lua"))()
     elseif GameID == Evade then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Evade.lua"))()
-    elseif GameID == BladeBall1 or GameID == BladeBall2 or GameID == BladeBall3 or GameID == BladeBall4 or GameID == BladeBall5 then
+    elseif 
+        GameID == BladeBall1 or GameID == BladeBall2 or GameID == BladeBall3 or 
+        GameID == BladeBall4 or GameID == BladeBall5 or GameID == BladeBall6 or 
+        GameID == BladeBall7 or GameID == BladeBall8 or GameID == BladeBall9 or 
+        GameID == BladeBall10 or GameID == BladeBall11 or GameID == BladeBall12 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Blade%20Ball.lua"))()
     else
         loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/IDMusic.lua"))()
