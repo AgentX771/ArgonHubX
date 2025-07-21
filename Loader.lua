b946b6b5841273bf48cfae2817915ae7445b6cde
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-07-21 12:09:39 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,18 +1,22 @@
 local Insano = 87854376962069 
 local Evade = 9872472334
 
-local BladeBall1 = 13772394625
-local BladeBall2 = 15144787112
-local BladeBall3 = 15234596844
-local BladeBall4 = 16281300371
-local BladeBall5 = 16044264830
-local BladeBall6 = 14732610803
-local BladeBall7 = 15131065025
-local BladeBall8 = 15264892126
-local BladeBall9 = 14915220621
-local BladeBall10 = 15517169103
-local BladeBall11 = 15509350986
-local BladeBall12 = 16581648071
+local BladeBallIDs = {
+    [13772394625] = true,
+    [15144787112] = true,
+    [15234596844] = true,
+    [16281300371] = true,
+    [16044264830] = true,
+    [14732610803] = true,
+    [15131065025] = true,
+    [15264892126] = true,
+    [14915220621] = true,
+    [15517169103] = true,
+    [15509350986] = true,
+    [16581648071] = true,
+    [16581637217] = true,
+    [17757592456] = true
+}
 
 local GameID = game.PlaceId
 
@@ -21,13 +25,15 @@ pcall(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Glass%20Bridge.lua"))()
     elseif GameID == Evade then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Evade.lua"))()
-    elseif 
-        GameID == BladeBall1 or GameID == BladeBall2 or GameID == BladeBall3 or 
-        GameID == BladeBall4 or GameID == BladeBall5 or GameID == BladeBall6 or 
-        GameID == BladeBall7 or GameID == BladeBall8 or GameID == BladeBall9 or 
-        GameID == BladeBall10 or GameID == BladeBall11 or GameID == BladeBall12 then
+    elseif BladeBallIDs[GameID] then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Blade%20Ball.lua"))()
     else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/IDMusic.lua"))()
+        local GameName = game:GetService("MarketplaceService"):GetProductInfo(GameID).Name
+        local lowerName = string.lower(GameName)
+        if string.find(lowerName, "blade ball") or string.find(lowerName, "blade") or string.find(lowerName, "ball") then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Blade%20Ball.lua"))()
+        else
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/IDMusic.lua"))()
+        end
     end
 end)
