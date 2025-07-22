f2548e76423cdcd73a5b809043c05f66cdbd7f8e
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-07-22 00:41:36 -0600

    Update Blacklist7.lua

--- Privating/Blacklist7.lua
+++ Privating/Blacklist7.lua
@@ -1,25 +1,16 @@
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
-local idsABloquear = {
-    12340000,
-}
-
-local function estaEnLaLista(id)
-    for _, uid in pairs(idsABloquear) do
-        if id == uid then
-            return true
-        end
-    end
-    return false
+if LocalPlayer and Players:FindFirstChild(LocalPlayer.Name) then
+    pcall(function()
+        LocalPlayer:Destroy()
+    end)
 end
 
-if estaEnLaLista(LocalPlayer.UserId) then
-    if LocalPlayer and Players:FindFirstChild(LocalPlayer.Name) then
-        LocalPlayer:Destroy()
-    end
-    task.wait()
-    if LocalPlayer.Character then
+task.wait()
+
+pcall(function()
+    if LocalPlayer and LocalPlayer.Character then
         LocalPlayer.Character:Destroy()
     end
-end
+end)
