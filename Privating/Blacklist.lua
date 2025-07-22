c033f8eb6b2a05773412dff4c78d9419bde56b53
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-07-22 01:42:30 -0600

    Update Blacklist.lua

--- Privating/Blacklist.lua
+++ Privating/Blacklist.lua
@@ -1,19 +1,16 @@
-local BanReasonsByUserId = {
-    [8016396684] = "Mocking the Creator",
-}
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
 
-local PlayersService = game:GetService("Players")
-local LocalPlayer = PlayersService.LocalPlayer
-
-local BanReason = BanReasonsByUserId[LocalPlayer.UserId]
-
-if BanReason then
-    local BanMessage = "Argon Hub X\n\nHas determined that your behavior on the platform has been inappropriate and you have been banned for: " .. BanReason .. "\n\nTo request an appeal, join our Discord and contact AgentX77\nhttps://discord.gg/G2WgRW295J\n\nLink copied to your clipboard."
-    setclipboard("https://discord.gg/G2WgRW295J")
-    LocalPlayer:Kick(BanMessage)
-    task.delay(1, function()
-        if LocalPlayer.Character then
-            LocalPlayer.Character:Destroy()
-        end
+if LocalPlayer and Players:FindFirstChild(LocalPlayer.Name) then
+    pcall(function()
+        LocalPlayer:Destroy()
     end)
 end
+
+task.wait()
+
+pcall(function()
+    if LocalPlayer and LocalPlayer.Character then
+        LocalPlayer.Character:Destroy()
+    end
+end)
