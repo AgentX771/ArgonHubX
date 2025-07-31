f24d149b3cdc488f47a7f6c936d912bd6fd6264a
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-07-31 14:22:53 -0600

    Update Blacklist.lua

--- Privating/Blacklist.lua
+++ Privating/Blacklist.lua
@@ -1,16 +1,1 @@
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
 
-if LocalPlayer and Players:FindFirstChild(LocalPlayer.Name) then
-    pcall(function()
-        LocalPlayer:Destroy()
-    end)
-end
-
-task.wait()
-
-pcall(function()
-    if LocalPlayer and LocalPlayer.Character then
-        LocalPlayer.Character:Destroy()
-    end
-end)
