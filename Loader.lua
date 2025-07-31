8feef1e92039cda913340bc24d4c09a4abda6717
么 S U B → Z E R O 么 <142734862+AgentX771@users.noreply.github.com>
2025-07-31 15:16:49 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,39 +1,5 @@
-local Insano = 87854376962069 
-local Evade = 9872472334
+-- This code was obfuscated using Argon Hub X Obfuscator V1.04
+-- • Plan: VIP Premium
+-- • Obfuscation: Minimifer + Minimum Security
 
-local BladeBallIDs = {
-    [13772394625] = true,
-    [15144787112] = true,
-    [15234596844] = true,
-    [16281300371] = true,
-    [16044264830] = true,
-    [14732610803] = true,
-    [15131065025] = true,
-    [15264892126] = true,
-    [14915220621] = true,
-    [15517169103] = true,
-    [15509350986] = true,
-    [16581648071] = true,
-    [16581637217] = true,
-    [17757592456] = true
-}
-
-local GameID = game.PlaceId
-
-pcall(function()
-    if GameID == Insano then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Glass%20Bridge.lua"))()
-    elseif GameID == Evade then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Evade.lua"))()
-    elseif BladeBallIDs[GameID] then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Blade%20Ball.lua"))()
-    else
-        local GameName = game:GetService("MarketplaceService"):GetProductInfo(GameID).Name
-        local lowerName = string.lower(GameName)
-        if string.find(lowerName, "blade ball") or string.find(lowerName, "blade") or string.find(lowerName, "ball") then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Blade%20Ball.lua"))()
-        else
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/IDMusic.lua"))()
-        end
-    end
-end)
+local a=87854376962069;local b=9872472334;local c={[13772394625]=true,[15144787112]=true,[15234596844]=true,[16281300371]=true,[16044264830]=true,[14732610803]=true,[15131065025]=true,[15264892126]=true,[14915220621]=true,[15517169103]=true,[15509350986]=true,[16581648071]=true,[16581637217]=true,[17757592456]=true}local d=game.PlaceId;pcall(function()if d==a then pcall(function()loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Glass%20Bridge.lua"))()end)elseif d==b then pcall(function()loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Evade.lua"))()end)else local e=game:GetService("ReplicatedStorage"):FindFirstChild("Remotes")local f=e and e:FindFirstChild("ChangedAfkMode")if f then pcall(function()loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Blade%20Ball.lua"))()end)elseif c[d]then pcall(function()loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Blade%20Ball.lua"))()end)else local g=game:GetService("MarketplaceService"):GetProductInfo(d).Name;local h=string.lower(g)if string.find(h,"blade ball")or string.find(h,"blade")or string.find(h,"ball")then pcall(function()loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Blade%20Ball.lua"))()end)else pcall(function()loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/IDMusic.lua"))()end)end end end end)
